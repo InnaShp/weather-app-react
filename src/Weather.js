14dc3b36797ceb16b1fcf3424c69ade37fba7e76
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-09 07:27:38 -0700

    Add some css to weather app

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SearchEngine from "./SearchEngine";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="container">
+    <div className="Weather container">
       <div className="card">
         <SearchEngine />
       </div>
