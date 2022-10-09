4f6ba21bbdf64b347803d23c04cb10593806435a
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-09 13:06:11 -0700

    Add functions for weather forecast

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -6,7 +6,9 @@ export default function SearchEngine() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
-  const [data, setData ] = useState("");
+  const [date, setDate ] = useState("");
+  const [weeklyForecast, setWeeklyForecast ] = useState("");
+  
 
   function formatDate(timestamp) {
     let date = new Date(timestamp);
@@ -30,7 +32,30 @@ export default function SearchEngine() {
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
-    setData(`${day} ${hours}:${minutes}`);
+    setDate(`${day} ${hours}:${minutes}`);
+  }
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+  }
+
+  function displayForecast(response) {
+    setWeeklyForecast(response.data.daily);
+    formatDay(weeklyForecast);
+  }
+
+  function getForecast(coordinates) {
+    let apiKey = "9ad78e7db9272efcf0a75aa55efdcd5a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   }
 
   function showWeather(response) {
@@ -44,18 +69,20 @@ export default function SearchEngine() {
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     });
     formatDate(response.data.dt * 1000);
+    getForecast(response.data.coord);
   }
 
-  function searchCity(city){
+  function searchCity(value){
     let apiKey = "9ad78e7db9272efcf0a75aa55efdcd5a";
     let units = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=${units}`;
     axios.get(url).then(showWeather);
   }
   function handleSubmit(event) {
     event.preventDefault();
     searchCity(city);
   }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
@@ -68,14 +95,14 @@ export default function SearchEngine() {
         </div>
         <div className="col-1">
           <button className="search" type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
           </button>
         </div>
         <div className="col-1">
           <button className="current">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-geo" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
             </svg>
           </button> 
@@ -89,11 +116,11 @@ export default function SearchEngine() {
       <div className="SearchEngine">
         {form}
         <div className="row">
-          <div className="col-6">
+          <div className="col-6 text-start">
             <h1>{weather.name}</h1>
-            <ul>
+            <ul className="text-muted">
               <li>
-                {data}
+                {date}
               </li>
               <li>
                 {weather.description}
@@ -108,6 +135,8 @@ export default function SearchEngine() {
             <h2><img src={weather.icon} alt="Weather icon" className="weather-icon" />{Math.round(weather.temperature)} <span className="celsium">°C</span></h2>
           </div>
         </div>
+        
+      
       </div>
     );
   } else {
